28783a28bef110a95079a471e8b36f6b5d41eb8a
burdms <burdms@gmail.com>
2021-09-21 15:03:35 +0200

    create: validation logic

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,9 +1,11 @@
 //= paymentMethod.js
+//= paymentValidate.js
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector('form');
 
   if (document.querySelector('.payment-method')) {
     paymentMethod(form);
+    paymentValidate(form);
   }
 });
---
+++ src/js/partials/paymentValidate.js
@@ -0,0 +1,90 @@
+function paymentValidate(form) {
+  const email = form.querySelector('input[data-type="email"]');
+  const payment = form.querySelector('.payment-method');
+  const paymentMethods = form.querySelectorAll('.payment-method__item input');
+
+  let error = false;
+
+
+  form.addEventListener('submit', e => {
+    let noMethod = true;
+
+    paymentMethods.forEach(item => {
+      if (item.checked) {
+        noMethod = false;
+      }
+    });
+
+    if (noMethod) {
+      error = true;
+    } else {
+      error = false;
+    }
+
+    if (error) {
+      e.preventDefault();
+      payment.classList.add('error');
+
+      setTimeout(() => {
+        payment.classList.remove('error');
+      }, 5000);
+    } else {
+      error = checkEmail(email);
+
+      if (error) {
+        e.preventDefault();
+        email.closest('.input').classList.add('error');
+
+        setTimeout(() => {
+          email.closest('.input').classList.remove('error');
+        }, 5000);
+      }
+    }
+  });
+
+  form.addEventListener('change', e => {
+    if (e.target.name) {
+      payment.classList.remove('error');
+    }
+  });
+
+  email.addEventListener('blur', () => {
+    error = checkEmail(email);
+
+    if (error) {
+      email.closest('.input').classList.add('error');
+
+      setTimeout(() => {
+        email.closest('.input').classList.remove('error');
+      }, 5000);
+    } else {
+      email.closest('.input').classList.remove('error');
+    }
+  });
+
+  function checkEmail(email) {
+    const caption = email.closest('.input').querySelector('.error-caption__text');
+
+    if (email.value.trim()) {
+      const value = email.value.trim();
+
+      if (!/^\w+@\w+\.\w{2,}$/.test(value)) {
+        caption.textContent = 'Некорректный email';
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      caption.textContent = 'Введи email';
+      return true;
+    }
+  }
+
+  function checkEmailOnInput(item) {
+    item.addEventListener('input', e => {
+      e.target.value = e.target.value.replace(/[^A-Z1-9@\-_.]/gi, '');
+    });
+  }
+
+  checkEmailOnInput(email);
+}
