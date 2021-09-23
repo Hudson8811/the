a878daebc53f129dec395a1ba5b1aa71267c5ea6
burdms <burdms@gmail.com>
2021-09-23 09:29:18 +0200

    apple test 5 (no payment)

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (document.querySelector('.payment-form')) {
     const form = document.querySelector('form');
 
-    // paymentMethod(form);
-    // paymentValidate(form);
+    paymentMethod(form);
+    paymentValidate(form);
   }
 });
